7d739288dea239b8a3048742b069ef1709545c2d
tardes <projoradio@gmail.com>
2025-04-30 19:32:36 +0200

    finish

---
+++ app/src/main/java/com/example/listadetareas/Task.kt
@@ -0,0 +1,8 @@
+package com.example.listadetareas
+
+data class Task(
+    val id: Long,
+    var title:String,
+    var done: Boolean
+
+)
--- app/src/main/java/com/example/listadetareas/dfdxftgh.kt
+++
@@ -1,4 +0,0 @@
-package com.example.listadetareas
-
-class dfdxftgh {
-}
